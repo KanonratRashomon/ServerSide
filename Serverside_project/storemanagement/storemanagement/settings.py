3caeb923f539c7a62c0f732452b7ad1d59e9a529
Rashomon <65070007@kmitl.ac.th>
2024-10-06 14:15:59 +0700

    homepageEdit1

--- Serverside_project/storemanagement/storemanagement/settings.py
+++ Serverside_project/storemanagement/storemanagement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'store',
 ]
 
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
